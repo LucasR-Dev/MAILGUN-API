e2fc000e6113b4779c9b2ec8abd7c3e9da466c37
Lucas Ribeiro <lucas.alves@braip.com>
2023-11-21 23:12:00 -0300

    feat: criação das variáveis para o mailgun

---
+++ config/notification.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    /**
+     * Mailgun Configurations
+     */
+    'mailgun' => [
+        'domain_name' => env('MAILGUN_DOMAIN_NAME'),
+        'api_key' => env('MAILGUN_API_KEY')
+    ],
+];
